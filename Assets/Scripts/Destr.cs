439c07730218331fe8f7188ef2c3b2a21b6eacb8
Volkov_la <Loewolf.argh@gmail.com>
2020-01-13 21:26:26 +0300

    New wall

---
+++ Assets/Scripts/Destr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destr : MonoBehaviour
+{
+    public Transform destroyed;
+    public void Dead()
+    {
+        Instantiate(destroyed, transform.position + new Vector3(0,-2,0), Quaternion.Euler(0, 0, 0));
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public float Radius;
+    public float Force;
+
+    void Update()   
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, Radius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i].GetComponent<Destr>())
+            {
+                hitColliders[i].GetComponent<Destr>().Dead();
+            }
+            if (hitColliders[i].CompareTag("CanBer"))
+            {
+                hitColliders[i].gameObject.AddComponent<Rigidbody>();
+            }
+        }
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+
+}
