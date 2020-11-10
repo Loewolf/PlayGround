2222673f68c4e039ef419803b09bd24dadc225eb
Loewolf <loewolf.argh@gmail.com>
2020-11-10 19:56:16 +0300

    First class new
    ...

--- Assets/Scripts/Education/Tasks/Move.cs
+++ Assets/Scripts/Education/Tasks/Move.cs
@@ -2,10 +2,11 @@
 
 public class Move : Task
 {
-    
+
     // Start is called before the first frame update
     public bool showInstructions = true; // При false отключает отображение всех инструкций для этого задания
     [Header("Объекты, связанные с задачей")]
+    public bool autr;
     public KeyCode[] key;
     public float time = 1f;
     private float timeEnd;
@@ -19,28 +20,47 @@ public class Move : Task
     {
     }
 
-    protected override int Task_0() // Присоединить оборудование
+    protected override int Task_0()
     {
-        bool t = false;
-        for (int i = 0; i < key.Length; i++)
-            if (Input.GetKey(key[i]))
-                t = true;
-        if (t)
+        if (mainGameObject.GetAllowRotation() == autr)
         {
-            if (timeEnd > 0)
-                timeEnd -= Time.deltaTime;
-            else
-            {
-                SetStage(1, Task_1, showInstructions);
-                return 1;
-            }
+            SetStage(1, Task_1, true);
+            return 1;
         }
         return 0;
+
     }
-    private int Task_1()
+
+    protected int Task_1() // Присоединить оборудование
+    {
+        if (mainGameObject.GetAllowRotation() == autr)
+        {
+            bool t = false;
+            for (int i = 0; i < key.Length; i++)
+                if (Input.GetKey(key[i]))
+                    t = true;
+            if (t)
+            {
+                if (timeEnd > 0)
+                    timeEnd -= Time.deltaTime;
+                else
+                {
+                    SetStage(2, EndTask, showInstructions);
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        else
+        {
+            SetStage(0, Task_0, showInstructions);
+            return 1;
+        }
+    }
+    private int Task_3()
     {
         if (Input.GetKey(KeyCode.Escape))
-            SetStage(1, EndTask, showInstructions);
+            SetStage(2, EndTask, showInstructions);
         return 0;
     }
 
--- Assets/Scripts/Education/Tasks/MoveArrow.cs
+++ Assets/Scripts/Education/Tasks/MoveArrow.cs
@@ -20,7 +20,7 @@ public class MoveArrow : Task
     {
         joinpoint = Instantiate(joinPoint);
         joinpoint.gameObject.SetActive(true);
-        
+
     }
 
     void Child(Transform parent)
@@ -62,35 +62,47 @@ public class MoveArrow : Task
         }
         else return 0;
     }
-    protected  int Task_1() // проверка на совпадение конфигураций
+    protected int Task_1()
     {
-        bool[] t = new bool[7];
-        for (int j = 0; j < 7; j++)
+        if (mainGameObject.GetAllowRotation())
         {
-            float dist1 = Vector3.Distance(mainArm[j].localPosition, tempArm[j].localPosition);
-            float dist2 = Quaternion.Angle(mainArm[j].localRotation, tempArm[j].localRotation) / 1000;
-
-            if (dist1 + dist2 < 0.02f)
-                t[j] = true;
-            else
-                t[j] = false;
-
-        }
-        bool tm = true;
-        for (int j = 0; j < 7; j++)
-            tm = tm && t[j];
-        if (tm)
-        {
-            SetStage(2, Task_2, showInstructions);
+            SetStage(2, Task_2, true);
             return 1;
         }
         return 0;
     }
-    private int Task_2()
+
+    protected int Task_2() // проверка на совпадение конфигураций
     {
-        if (Input.GetKey(KeyCode.Escape))
-            SetStage(3, EndTask, showInstructions);
-        return 0;
+        if (mainGameObject.GetAllowRotation())
+        {
+            bool[] t = new bool[7];
+            for (int j = 0; j < 7; j++)
+            {
+                float dist1 = Vector3.Distance(mainArm[j].localPosition, tempArm[j].localPosition);
+                float dist2 = Quaternion.Angle(mainArm[j].localRotation, tempArm[j].localRotation) / 1000;
+
+                if (dist1 + dist2 < 0.02f)
+                    t[j] = true;
+                else
+                    t[j] = false;
+
+            }
+            bool tm = true;
+            for (int j = 0; j < 7; j++)
+                tm = tm && t[j];
+            if (tm)
+            {
+                SetStage(3, EndTask, showInstructions);
+                return 1;
+            }
+            return 0;
+        }
+        else
+        {
+            SetStage(1, Task_1, showInstructions);
+            return 1;
+        }
     }
 
     // Update is called once per frame
